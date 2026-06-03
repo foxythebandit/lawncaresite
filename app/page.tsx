3ffbb6e07bbc3737752bdfad2fd5a178ce8331c1
Paxton Reynolds <paxonearth.22@gmail.com>
2026-06-03 01:07:39 -0500

    Revert "Add SVG wave dividers between sections"
    This reverts commit 4ee2eebca896753d1c52c82bffd8b519ad5c31d4.

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ import NoiseComparison from '@/components/NoiseComparison'
 import Testimonials from '@/components/Testimonials'
 import FAQ from '@/components/FAQ'
 import MapQuoteBuilder from '@/components/MapQuoteBuilder'
-import WaveDivider from '@/components/WaveDivider'
 import Footer from '@/components/Footer'
 
 export default function Home() {
@@ -19,15 +18,10 @@ export default function Home() {
       <Hero />
       <TrustBar />
       <MapQuoteBuilder />
-      <WaveDivider from="#1a3a2a" to="#f7f6f2" />
       <HowItWorks />
-      <WaveDivider from="#f7f6f2" to="#1a3a2a" />
       <WhyElectric />
-      <WaveDivider from="#1a3a2a" to="#f7f6f2" />
       <NoiseComparison />
-      <WaveDivider from="#f7f6f2" to="#f0faf4" />
       <Testimonials />
-      <WaveDivider from="#f0faf4" to="#f7f6f2" />
       <FAQ />
       <Footer />
     </>
